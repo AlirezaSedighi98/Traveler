9f85be73bd2ddf68217dee13ab4f777c868a7a73
Alireza Sedighi <alireza.sedighi.job@gmail.com>
2023-03-28 17:53:03 +0330

    mui configues

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { navLinks } from "./navLinks";
+import { IconButton } from "@mui/material";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import Button from "@mui/material/Button";
 
 const Navbar = () => {
   const leftLinks = navLinks.slice(0, Math.ceil(navLinks.length / 2));
@@ -18,11 +21,16 @@ const Navbar = () => {
       <NavLink to={"/"} className="logoImage">
         <img className="h-full" src="./images/logos/webLogo.png" alt="logo" />
         <h1 className="navHeader">Travler</h1>
-      </NavLink>  
+      </NavLink>
+      <div className="md:hidden">
+        <IconButton onClick={() => {}}>
+          <MenuRoundedIcon color="primary" fontSize="large" />
+        </IconButton>
+      </div>
       <div className="navLinks">
         {rightLinks.map((item, index) => (
           <NavLink className="links" key={index} to={item.link}>
-            {item.title} 
+            {item.title}
           </NavLink>
         ))}
       </div>
