f6ce182c717ed0ea1391ce06984e55ca315e196a
Alireza Sedighi <alireza.sedighi.job@gmail.com>
2023-05-03 11:23:26 +0330

    test

--- src/layout/carousel/Carousel.tsx
+++ src/layout/carousel/Carousel.tsx
@@ -4,12 +4,12 @@ const Carousel = () => {
   return (
     <div className="carouselContainer">
       <div className="carouselDescription">
-        <div className="carouselDescriptionField">
-          <h1></h1>
-          <div></div>
+        <div className="carouselDescriptionField w-2/3">
+          <h1 className="text-xl">New York</h1>
+          <div>picture of times square picture of times squarepicture of times squarepicture of times squarepicture of times squarepicture of times square</div>
         </div>
         <div className="carouselDescriptionField">
-          <h3></h3>
+          <h3>quote of the day</h3>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     fontSize: {
       base: "26px",
+      xl: "32px",
     },
     extend: {
       fontFamily: {
